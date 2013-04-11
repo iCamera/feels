14ad187c122fd5b810f1651c27aeb3abeaa79aeb
Andreas Areschoug <andreas.areschoug@monterosa.se>
2013-04-11 20:23:03 +0200

    init project

---
+++ feels/feels/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  feels
+//
+//  Created by Andreas Areschoug on 2013-04-11.
+//  Copyright (c) 2013 Feels. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
