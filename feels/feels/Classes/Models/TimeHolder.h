f4379008e0c7419a417585179058201e6d43b930
hellozimi <hello@zimi.se>
2013-04-12 21:40:23 +0200

    added time holder

---
+++ feels/feels/Classes/Models/TimeHolder.h
@@ -0,0 +1,15 @@
+//
+//  TimeHolder.h
+//  feels
+//
+//  Created by Simon Andersson on 4/12/13.
+//  Copyright (c) 2013 Feels. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TimeHolder : NSObject
+@property (nonatomic, assign) double delay;
+@property (nonatomic, assign) double offset;
+@property (nonatomic, assign) double time;
+@end
---
+++ feels/feels/Classes/Models/TimeHolder.m
@@ -0,0 +1,13 @@
+//
+//  TimeHolder.m
+//  feels
+//
+//  Created by Simon Andersson on 4/12/13.
+//  Copyright (c) 2013 Feels. All rights reserved.
+//
+
+#import "TimeHolder.h"
+
+@implementation TimeHolder
+
+@end
